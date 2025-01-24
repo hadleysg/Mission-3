f3bbda31492e8906d1bfaad5ab804aff173a1b76
hadleysg <hadleygarff@gmail.com>
2025-01-23 20:23:38 -0700

    Add project files.

---
+++ FoodItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission_3
+{
+    // This creates placeholders for the values. 
+    public class FoodItem
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string ExpirationDate { get; set; }
+
+
+        // This is a constructor that neatley assigns values every time that you create a new FoodItem
+        public FoodItem(string name, string category, int quantity, string expirationDate)
+        {
+            Name = name;
+            Category = category;        
+            Quantity = quantity;
+            ExpirationDate = expirationDate;
+
+        }
+
+        // Override ToString for easy display
+        public override string ToString()
+        {
+            return $"Name: {Name}, Category: {Category}, Quantity: {Quantity}, Expiration Date: {ExpirationDate}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// Hadley Garff 4-14
+// This program will manage an inventory of food items for a local food bank
+// The user should be able to add, delete, and print the food items
+
+using Mission_3;
+
+// Declaration and Installation of FoodItem
+FoodItem food;
+food = new FoodItem();
+
+// Here we are informing the user how to manage the inventory
+Console.WriteLine("Welcome to the Food Bank Inventory!\n");
+Console.WriteLine("Would you like to:\n1. Add Food Items\n2. Delete Food Items\n3. Print List of Current Food Items\n4. Exit the Program\n");
+Console.WriteLine("Please enter the corresponding number.");
+
+// Based on the number they type, a function will run
+int actionChoice = 0;
+actionChoice = Console.ReadLine();
+
+
+if (actionChoice == "1")
+{
+    addFoodItem();
+}
+else if (actionChoice == "2")
+{
+    deleteFoodItem();
+}
+else if (actionChoice == "3")
+{
+    printFoodItems();
+}
+else if (actionChoice == "4")
+{
+    Console.WriteLine("Exiting the program. Goodbye!");
+    return;
+}
+else
+{
+    Console.WriteLine("Invalid choice. Please try again.");
+}
+
+static void AddFoodItem()
+{
+
+}
